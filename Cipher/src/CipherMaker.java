0968aa86d3be4ea1ffd35e5a7045fe012289dae4
DonovanCo <donovanco@mullenmail.com>
2015-02-02 21:11:00 -0700

    More Work

--- Cipher/src/CipherMaker.java
+++ Cipher/src/CipherMaker.java
@@ -1,11 +1,88 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class CipherMaker
+public class CipherMaker extends Key
 	{
+	static String[][] cipherSquare = new String[26][26];
+	static String message;
+	static String myKey;
 
+	static Scanner file = new Scanner(System.in);
+	
 	public static void main(String[] args)
 		{
-		Key.makeKey();
+		makeKey();
 
+		}
+	
+	public static void makeKey()
+	{
+	String []alphabetArray = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z"};
+	ArrayList<String> myList = new ArrayList<String>();
+	for(int q = 0; q<alphabetArray.length; q++)
+		{
+		myList.add(alphabetArray[q]);
+		}
+	
+	for(int i = 0; i<myList.size(); i++)
+		{
+		for(int x = 0; x<myList.size(); x++)
+			{
+			cipherSquare[i][x]=myList.get(x).toString();
+			}
+		String temp = myList.get(0).toString();
+		myList.remove(myList.get(0));
+		myList.add(temp);
+		myList.set(myList.size()-1, temp);
+		}
+	}
+	
+	public static void ask()
+		{
+		System.out.println("Would you like to 1.) Encrypt or 2.) Decrypt a message.");
+		int choice = file.nextInt();
+		if(choice == 1)
+			{
+			 encrypt();
+			}
+		else if(choice == 2)
+			{
+			 decrypt();
+			}
+		else
+			{
+			System.out.println("That is not an option.");
+			}
+		}
+	
+	public static void encrypt()
+	{
+		myKey = getKey().toLowerCase();
+		message = getMessage().toLowerCase();
+	}
+		
+	
+	public static String getKey()
+		{
+		System.out.println("Enter in your key.");
+		String str = file.next();
+		return str;
+		}
+	
+	public static String getMessage()
+		{
+		System.out.println("Enter in your message without spaces.");
+		String str = file.next();
+		return str;
+		}
+	
+	public static String makeEncryption()
+		{
+		int keyInd = 0;
+		for(int i = 0; i<message.length(); i++)
+			{
+			for(int x  = 0; x<)
+			}
 		}
 
 	}
--- Cipher/src/Key.java
+++ Cipher/src/Key.java
@@ -4,35 +4,6 @@ import java.util.ArrayList;
 public class Key
 	{
 	
-	public static void makeKey()
-		{
-		String []alphabetArray = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z"};
-		ArrayList myList = new ArrayList();
-		for(int q = 0; q<alphabetArray.length; q++)
-			{
-			myList.add(alphabetArray[q]);
-			}
-		
-		String[][] cipherSquare = new String[26][26];
-		for(int i = 0; i<alphabetArray.length; i++)
-			{
-			for(int x = 0; x<alphabetArray.length; x++)
-				{
-				cipherSquare[i][x]=myList.get(x).toString();
-				}
-			String temp = myList.get(i).toString();
-			myList.remove(myList.get(0));
-			myList.add(temp);
-			myList.set(myList.size()-1, temp);
-			}
-		for(int z = 0; z<cipherSquare.length; z++)
-			{
-			for(int a = 0; a<cipherSquare[0].length; a++)
-				{
-				System.out.print(cipherSquare[z][a]);
-				}
-			System.out.println();
-			}
-		}
+	
 	
 	}
